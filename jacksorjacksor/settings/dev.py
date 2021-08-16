c4f38607d8d1e1a83e5c83448f0bdf7f0182e017
jacksorjacksor <rich@richardjacksonmusic.com>
2021-08-16 16:34:09 +0100

    Having issues with deploying to PA, have logged w support

--- jacksorjacksor/settings/dev.py
+++ jacksorjacksor/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-iq=90lh(-(!6r&h^$67g066_c#5n$u(s3@3x*gt^7*+21#^7pr'
