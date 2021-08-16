d70900c15cfbab0ab36bf38a619e9f15c46b9e20
jacksorjacksor <rich@richardjacksonmusic.com>
2021-08-16 16:04:53 +0100

    changing settings (dev.py)

--- jacksorjacksor/settings/dev.py
+++ jacksorjacksor/settings/dev.py
@@ -1,13 +1,13 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-iq=90lh(-(!6r&h^$67g066_c#5n$u(s3@3x*gt^7*+21#^7pr'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['jacksorjacksor.eu.pythonanywhere.com', 'localhost', ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
