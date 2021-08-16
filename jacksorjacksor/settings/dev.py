78305d29baca9a19011c97a0722206ad69d26b83
jacksorjacksor <rich@richardjacksonmusic.com>
2021-08-16 15:47:11 +0100

    initial commit

---
+++ jacksorjacksor/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-iq=90lh(-(!6r&h^$67g066_c#5n$u(s3@3x*gt^7*+21#^7pr'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
