3ff849c6dfd11e18cbfd969b0c4ce62cd34d279b
jacksorjacksor <rich@richardjacksonmusic.com>
2021-08-17 10:20:30 +0100

    Added a production secret key

--- jacksorjacksor/settings/production.py
+++ jacksorjacksor/settings/production.py
@@ -6,6 +6,8 @@ ALLOWED_HOSTS = ['jacksorjacksor.eu.pythonanywhere.com']
 
 ROOT_URLCONF = 'jacksorjacksor.urls'
 
+SECRET_KEY = '%+2&g)dpydd(^m)@t#2vynm)wce-es4*mapas#^uj1-w464+'
+
 try:
     from .local import *
 except ImportError:
